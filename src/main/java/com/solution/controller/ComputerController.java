ff153d98f1a53dff853c0e23166cada52b62c4ae
vinodh1988 <k.c.vinodh@hotmail.com>
2025-06-25 10:34:36 +0530

    put

--- src/main/java/com/solution/controller/ComputerController.java
+++ src/main/java/com/solution/controller/ComputerController.java
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.solution.entities.Computer;
 import com.solution.services.ComputerService;
 import com.solution.utilities.RecordAlreadyExistsException;
+import com.solution.utilities.RecordNotFoundException;
 
 @RestController
 @RequestMapping("/api/computers")
@@ -27,12 +30,29 @@ public class ComputerController {
 		return computerService.getComputers();
 	}
 	
+	@GetMapping("/{cno}")
+	public ResponseEntity<Computer> getComputerByCno(@PathVariable int cno) throws RecordNotFoundException
+	{
+		Computer computer = computerService.getComputerByCno(cno);
+		return ResponseEntity.status(HttpStatus.OK).body(computer);
+	}
+	
 	@PostMapping("")
-	public ResponseEntity<Object> addComputer(@RequestBody Computer computer) throws RecordAlreadyExistsException
+	public ResponseEntity<Computer> addComputer(@RequestBody Computer computer) throws RecordAlreadyExistsException
 	{
 
 		computerService.addComputer(computer);
 		return ResponseEntity.status(HttpStatus.CREATED).body(computer);
 		
+	}
+	
+	@RequestMapping(value="", method = {RequestMethod.PUT, RequestMethod.PATCH})
+	public ResponseEntity<String> updateComputer(@RequestBody Computer computer) throws RecordNotFoundException
+	{
+		if (computer.getCno() == null) {
+			throw new IllegalArgumentException("Cno must not be null for update operation");
+		}
+		computerService.updateComputer(computer);
+		return ResponseEntity.status(HttpStatus.OK).body("record updated successfully");
 	}
 }
--- src/main/java/com/solution/services/ComputerService.java
+++ src/main/java/com/solution/services/ComputerService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.solution.entities.Computer;
 import com.solution.repositories.ComputerRepository;
 import com.solution.utilities.RecordAlreadyExistsException;
+import com.solution.utilities.RecordNotFoundException;
 
 @Service
 public class ComputerService {
@@ -18,6 +19,16 @@ public class ComputerService {
 	  
 	return computerRepository.findAll();
   }
+  
+
+  
+  public Computer getComputerByCno(int cno) throws RecordNotFoundException {
+	 Computer c= computerRepository.findByCno(cno);
+	 if(c == null) 
+		 throw new RecordNotFoundException();
+	  return computerRepository.findByCno(cno);
+  }
+  
 //  /Save method is not only used for insertion but also updation
   public void addComputer(Computer computer) throws RecordAlreadyExistsException {
       Computer c= computerRepository.findByCno(computer.getCno());
@@ -25,5 +36,18 @@ public class ComputerService {
     	  throw new RecordAlreadyExistsException();
 	  computerRepository.save(computer);
   }
+  
+  public void updateComputer(Computer computer) throws RecordNotFoundException {
+	  Computer c= computerRepository.findByCno(computer.getCno());
+	  if(c == null) 
+		  throw new RecordNotFoundException();
+	  computer.setBrand(computer.getBrand()!=null ? computer.getBrand() : c.getBrand());
+      computer.setCpu(computer.getCpu()!=null ? computer.getCpu() : c.getCpu());
+      computer.setRam(computer.getRam()!=null ? computer.getRam() : c.getRam());
+	  computer.setDisksize(computer.getDisksize()!=null ? computer.getDisksize() : c.getDisksize());
+	  computerRepository.save(computer);
+  }
+  
+  
   
 }
